80d9165d2f62e0c2f31ff37f934d65add5eb446f
PatricioOsorio <patriciomiguel_12@hotmail.com>
2023-10-27 20:12:29 -0600

    Formulario agregado

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import RoutingRoutes from './RoutingRoutes';
 import Geolocation from './Geolocation';
+import Form from './Form';
 
 const Main = () => {
   return (
     <>
       <RoutingRoutes />
       <Geolocation />
+      <Form />
     </>
   );
 };
